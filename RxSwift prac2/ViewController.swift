b9cc435051547f3fc5328e070ddd78f1acc2d86e
Walkdream24 <miku.mei2032@docomo.ne.jp>
2018-10-22 22:16:15 +0900

    Initial Commit

---
+++ RxSwift prac2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RxSwift prac2
+//
+//  Created by 中重歩夢 on 2018/10/22.
+//  Copyright © 2018 Ayumu Nakashige. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
